0a2e76dd18083b1362ca0db45fb593832b7466f9
UnaryPlus <ombspring@gmail.com>
2023-06-26 23:16:06 -0400

    add documentation, use NonEmpty

--- src/Jaskell/Quote.hs
+++ src/Jaskell/Quote.hs
@@ -1,3 +1,46 @@
+{-|
+Module     : Jaskell.Quote
+Copyright  : (c) Owen Bechtel, 2023
+License    : MIT
+Maintainer : ombspring@gmail.com
+Stability  : experimental
+
+The 'jsl' quasiquoter converts Jaskell syntax into Haskell syntax.
+
+A Jaskell expression is a sequence of commands or "atoms." An atom is one of the following:
+
+* A Haskell identifier, optionally preceded by @$@, @#@, @?@, @!@, or @&@. 
+  The identifier can be qualified or unqualified, and can be lowercase (function) or uppercase (data constructor).
+  It cannot be an operator.
+
+    * An identifier @x@ translates to @x@ if @x@ is a function, and @'Jaskell.push' x@ if @x@ is a data constructor.
+    * @$x@ translates to @'Jaskell.liftS' x@. For example, @$reverse@ will reverse the list on top of the stack.
+    * @#x@ translates to @'Jaskell.liftS2' x@. For example, @#gcd@ will pop the top two values and push their gcd.
+    * @?x@ translates to @'Jaskell.pushM' x@. For example, @?getLine@ will execute 'getLine' and push the result.
+    * @!x@ translates to @'Jaskell.popM' x@. For example, @!putStrLn@ will print the string on top of the stack.
+    * @&x@ translates to @'Jaskell.liftSM' x@. For example, @&readFile@ will pop a file path and push the contents of that file.
+  
+* A Haskell operator. 
+  This translates to 'Jaskell.liftS2' applied to the operator.
+
+* A list of zero or more expressions, surrounded in square brackets and separated by commas.
+  For example, @[ 1, 3, 4 1 + ]@ pushes the list @[ 1, 3, 5 ]@ onto the stack.
+
+* A tuple of two expressions. 
+  For example, @( 0, "a" "b" ++ )@ pushes the tuple @( 0, "ab" )@ onto the stack.
+
+* An empty tuple. This translates to @'Jaskell.push' ()@.
+
+* An integer, floating-point, character, or string literal.
+  For example, @5@ translates to @'Jaskell.push' 5@.
+
+* A (potentially empty) expression surrounded in curly brackets. 
+  This pushes the translation of the expression onto the stack, allowing for higher-order programming.
+
+Jaskell programs can be preceded by zero or more definitions, each of the form @DEF name = expr ;@. 
+Definitions can be recursive and even mutually recursive.
+-}
+
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Jaskell.Quote 
@@ -8,12 +51,12 @@ module Jaskell.Quote
   , Parser, parseName, parseLiteral, parseAtom, parseExpr, parseProgram
   ) where
 
--- TODO: add module description
-
 import Data.Void (Void)
 import Control.Monad (void)
-import Control.Category ((>>>))
 import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
+import Data.List.NonEmpty (NonEmpty((:|)))
+import Control.Category ((>>>))
+import qualified Control.Category as Cat
 
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Char as C
@@ -26,6 +69,7 @@ import Language.Haskell.TH.Lib
 import qualified Jaskell
 import qualified Jaskell.Prelude as Pre
 
+-- | Embed a Jaskell program into Haskell.
 jsl :: QuasiQuoter
 jsl = QuasiQuoter 
   { quoteExp = quote 
@@ -33,7 +77,7 @@ jsl = QuasiQuoter
   , quoteType = undefined
   , quoteDec = undefined 
   }
-  
+
 data NameMode
   = Bare
   | LiftS  
@@ -61,11 +105,11 @@ data Atom
   | Op String
   | List [Expr]
   | Tup Expr Expr
-  | Quote Expr
+  | Quote (Maybe Expr)
   | Lit Literal 
   deriving (Eq, Show)
 
-newtype Expr = Expr [Atom]
+newtype Expr = Expr (NonEmpty Atom)
   deriving (Eq, Show)
 
 data Program = Program [(String, Expr)] Expr
@@ -160,14 +204,14 @@ parseAtom = M.choice
   , Op <$> parseOp <* spaces
   , parseList 
   , parseTup 
-  , Quote <$ symbol '{' <*> optionalExpr <* symbol '}'
+  , Quote <$ symbol '{' <*> M.optional parseExpr <* symbol '}'
   , Lit <$> parseLiteral <* spaces
   ]
-  where
-    optionalExpr = M.choice [ parseExpr, return (Expr []) ]
 
 parseExpr :: Parser Expr
-parseExpr = Expr <$> M.some parseAtom
+parseExpr = do
+  atoms <- (:|) <$> parseAtom <*> M.many parseAtom 
+  return (Expr atoms)
 
 parseDef :: Parser (String, Expr)
 parseDef = (,) <$ M.chunk "DEF" <* spaces <*> lowerName <* spaces <* symbol '=' <*> parseExpr <* symbol ';'
@@ -203,7 +247,7 @@ quote input = do
     Left errors -> fail (M.errorBundlePretty errors)
     Right prog -> convertProgram prog
 
-comp :: [ExpQ] -> ExpQ
+comp :: Foldable t => t ExpQ -> ExpQ
 comp = foldr1 (\f g -> infixE (Just f) (varE '(>>>)) (Just g))
 
 convertName :: Name -> ExpQ
@@ -242,12 +286,12 @@ convertAtom = \case
   
   Tup x1 x2 -> comp [ convertExpr x1, convertExpr x2, varE 'Pre.pair ]
   
-  Quote x -> appE (varE 'Jaskell.push) (convertExpr x)
+  Quote x -> appE (varE 'Jaskell.push) (maybe (varE 'Cat.id) convertExpr x)
   
   Lit lit -> appE (varE 'Jaskell.push) (convertLiteral lit)
 
 convertExpr :: Expr -> ExpQ
-convertExpr (Expr xs) = comp (map convertAtom xs)
+convertExpr (Expr xs) = comp (fmap convertAtom xs)
 
 convertDef :: (String, Expr) -> DecQ
 convertDef (n, x) = funD (TH.mkName n) [ clause [] (normalB (convertExpr x)) [] ]
